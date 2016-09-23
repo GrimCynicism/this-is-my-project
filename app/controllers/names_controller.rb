437cf7e81fba298fbbdf9ee290c549283a1709d4
Scott Gibson <sevgibson@gmail.com>
2016-09-23 17:18:58 -0400

    Whateva

---
+++ app/controllers/names_controller.rb
@@ -0,0 +1,26 @@
+class NamesController < ApplicationController
+  
+  def index
+  	@names = Name.all
+  end
+
+  def new
+  	@name = Name.new
+  end
+
+  def create
+  	@name = Name.new(name_params)
+  	if @name.save
+  		render :index
+  	else
+  		render :new
+  	end
+  end
+
+  private
+
+  def name_params
+  	params.require(:name).permit(:name)
+  end
+
+end
